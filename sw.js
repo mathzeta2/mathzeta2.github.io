9823d7e5207448c42e2ff88a285e0b48f2ccf340
mathzeta2 <3099957622@qq.com>
2025-10-04 21:52:40 +0800

    Site updated: 2025-10-04 21:52:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759584228854";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759585953600";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
